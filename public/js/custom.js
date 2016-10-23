113faeda7aca297e316ba4255be1ce0b9f8037ea
Jenny Sergent <jennyse@gmail.com>
2016-10-23 20:12:21 +0200

    Posting the form to google via ajax. There's client-side validation, but this doesn't protect us from spam bots or evil people. More security needs to get in place!

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,16 +1,16 @@
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length,c.length);
+    }
+  }
+  return "";
 }
 
 if (getCookie('listmiCookie').length > 0) {
@@ -26,4 +26,39 @@ $( "#closeDisclaimerBtn" ).click(function() {
     $('#cookieDisclaimerConfirmation').delay(2000).fadeOut(600, 'linear');
   });
 
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function postForm() {
+  var email = $('#email').val();
+  var accepted = $('#acceptConditions').prop('checked');
+  if ((accepted) && ((email !== "") && (validateEmail(email)))) {
+    $.ajax({
+      url: "https://docs.google.com/a/matkvitton.se/forms/d/e/1FAIpQLSchMng-DVpm1WCxrOa_i73fzCqj_P9BxJ1MW6DhYxoqQbxQug/formResponse",
+      data: {"emailAddress" : email},
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function (){
+          window.location = location.host + "/tack/";
+        },
+        200: function (){
+          window.location = location.host + "/tack/";
+        }
+      }
+    });
+  } else {
+    alert("Oj! Nu blev det knas! Prova att försöka igen och om felet inte ger med sig får du skicka oss ett mail (good luck with that!).");
+  }
+}
+
+$(document).ready(function(){
+  $('#intresseanmalan').submit(function() {
+    postForm();
+    return false;
+  });
 });
