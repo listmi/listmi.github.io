f286f151d688861103ef949f18669fb54ebdf1db
Jenny Sergent <jennyse@gmail.com>
2017-03-25 21:04:13 +0100

    Nytt formulär för att anmäla sitt intresse som beta-testare

--- public/js/custom.js
+++ public/js/custom.js
@@ -28,9 +28,15 @@ $( "#closeDisclaimerBtn" ).click(function() {
 
 });
 
-function validateEmail(email) {
+function validateEmail(adress) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  return re.test(adress);
+}
+
+// Testar enbart början på adressen
+function validateGmail(adress) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))$/; // @gmail.([a-zA-Z]{2,})
+  return re.test(adress);
 }
 
 function postForm() {
@@ -57,9 +63,42 @@ function postForm() {
   }
 }
 
+function postBetaForm() {
+  var email = $('#email').val();
+  var os = $("input[type='radio'][name='operativsystem']:checked").val();
+  var accepted = $('#acceptConditions').prop('checked');
+  var tackSida = location.protocol + '//'+ location.host + "/tack/";
+  if ((accepted) && ((email !== "") && (validateGmail(email)))) {
+    $.ajax({
+      url: "https://docs.google.com/a/matkvitton.se/forms/d/e/1FAIpQLSewbUqJUguTseUaQvJeAyzxEt1evb60zGzVbjMuPXnEmQLuEA/formResponse",
+        data: {
+          "emailAddress" : email+'@gmail.com',
+          "entry.1102873716": os
+      },
+      type: "POST",
+      dataType: "xml",
+      crossDomain: true,
+      statusCode: {
+        200: function() {
+          window.location = tackSida;
+        },
+        0: function (){
+          window.location = tackSida;
+        }
+      }
+    });
+  } else {
+    $('#errorMessage').show(); // visar felmeddelande
+  }
+}
+
 $(document).ready(function(){
   $('#intresseanmalan').submit(function() {
     postForm();
+    return false;
+  });
+  $('#intresseanmalan-beta').submit(function() {
+    postBetaForm();
     return false;
   });
 });
